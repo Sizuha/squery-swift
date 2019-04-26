444b99d18891ae6dd65214121b09017cc97fd714
IL KYOUNG HWANG <sizuha@gngs.co.jp>
2019-04-26 10:42:00 +0900

    ~ rename Test class

--- SQueryTests/SQueryTests.swift
+++ SQueryTests/SQueryTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import SQuery
 
-class squeryTests: XCTestCase {
+class SQueryTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
