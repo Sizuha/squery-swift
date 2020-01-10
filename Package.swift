d7bf5c7e4370922e039ee56401f6232e66be6a8b
IL KYOUNG HWANG <sizuha@gngs.co.jp>
2020-01-10 14:12:25 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SQuery",
-            targets: ["squery"]),
+            targets: ["SQuery"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "squery",
+            name: "SQuery",
             dependencies: []),
         .testTarget(
-            name: "squeryTests",
-            dependencies: ["squery"]),
+            name: "SQueryTests",
+            dependencies: ["SQuery"]),
     ]
 )
